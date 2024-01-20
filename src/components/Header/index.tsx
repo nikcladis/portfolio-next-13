728b59f59ddb4a4ed3875cac5b64484f33fe04b8
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-20 23:27:15 +0200

    added header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import Container from "@/components/ui/container";
+import { Button } from "@/components/ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { ThemeToggle } from "@/components/theme/theme-toggle";
+
+const Header = () => {
+  return (
+    <header className="sm:flex sm:justify-between py-3 px-4 border-b">
+      <Container>
+        <div className="flex items-center justify-between">
+          <h1 className="font-bold">nikcladis.dev</h1>
+          <div className="flex items-center">
+            <Button variant="ghost" size="icon">
+              <a
+                href="https://www.github.com/nikcladis"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
+              </a>
+            </Button>
+            <ThemeToggle />
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
