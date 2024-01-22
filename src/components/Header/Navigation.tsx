304e3ad10384ba8cd0518d96eeca747db732567e
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-22 17:14:00 +0200

    added x icon

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { FaXTwitter } from "react-icons/fa6";
 import { ThemeToggle } from "@/components/theme/theme-toggle";
 import { Menu } from "lucide-react";
 import { motion, useScroll, useMotionValueEvent } from "framer-motion";
@@ -35,6 +36,15 @@ const Navigation = () => {
         <Menu />
       </Button>
       <div className="flex items-center">
+        <Button variant="ghost" size="icon">
+          <a
+            href="https://www.twitter.com/nikcladis"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaXTwitter className="h-[1.2rem] w-[1.2rem]" />
+          </a>
+        </Button>
         <Button variant="ghost" size="icon">
           <a
             href="https://www.github.com/nikcladis"
