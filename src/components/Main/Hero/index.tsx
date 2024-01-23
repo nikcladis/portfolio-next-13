826f149992d64c91c7e5a2a2561f80a55cfb4f41
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-23 04:49:44 +0200

    removed text animation

--- src/components/Main/Hero/index.tsx
+++ src/components/Main/Hero/index.tsx
@@ -4,24 +4,24 @@ import { useLayoutEffect, useState } from "react";
 import { debounce } from "lodash";
 
 const Hero = () => {
-  const [scrollY, setScrollY] = useState(0);
-  const controls = useAnimation();
+  // const [scrollY, setScrollY] = useState(0);
+  // const controls = useAnimation();
 
-  const handleScroll = debounce(() => {
-    setScrollY(window.scrollY);
-  }, 50);
+  // const handleScroll = debounce(() => {
+  //   setScrollY(window.scrollY);
+  // }, 50);
 
-  useLayoutEffect(() => {
-    controls.start({ opacity: 1 - scrollY / 150 });
-  }, [scrollY, controls]);
+  // useLayoutEffect(() => {
+  //   controls.start({ opacity: 1 - scrollY / 150 });
+  // }, [scrollY, controls]);
 
-  useLayoutEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+  // useLayoutEffect(() => {
+  //   window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [handleScroll]);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, [handleScroll]);
 
   return (
     <section className="fixed inset-0 z-0 flex flex-col justify-end h-[90svh] mx-auto w-full max-w-7xl px-4 xl:px-0 overflow-hidden">
@@ -32,19 +32,19 @@ const Hero = () => {
           transition={{ delay: 0.3, duration: 0.5 }}
           className="block mt-5 text-[6rem] md:text-[8rem]"
         >
-          <span className="relative">
+          {/* <span className="relative">
             <motion.span
               initial={{ opacity: 1 }}
               animate={controls}
               className="absolute z-10 top-0 bottom-0"
-            >
-              Hello,
-            </motion.span>
-            <span className="absolute z-0 top-0 bottom-0 text-red-500">
-              Hell
-            </span>
-          </span>
-          &#8203;
+            > */}
+          Hello,
+          {/* </motion.span> */}
+          {/* <span className="absolute z-0 top-0 bottom-0 text-red-500"> */}
+          {/* Hell */}
+          {/* </span> */}
+          {/* </span> */}
+          {/* &#8203; */}
         </motion.span>
         <motion.span
           initial={{ x: 500, opacity: 0 }}
