4f997ddbb268b236b4fea301a360c36b245cae10
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-23 19:40:59 +0200

    changed the font sizes a little bit

--- src/components/Main/Hero/index.tsx
+++ src/components/Main/Hero/index.tsx
@@ -4,64 +4,49 @@ import { useLayoutEffect, useState } from "react";
 import { debounce } from "lodash";
 
 const Hero = () => {
-  // const [scrollY, setScrollY] = useState(0);
-  // const controls = useAnimation();
+  const [scrollY, setScrollY] = useState(0);
+  const controls = useAnimation();
 
-  // const handleScroll = debounce(() => {
-  //   setScrollY(window.scrollY);
-  // }, 50);
+  const handleScroll = debounce(() => {
+    setScrollY(window.scrollY);
+  }, 50);
 
-  // useLayoutEffect(() => {
-  //   controls.start({ opacity: 1 - scrollY / 150 });
-  // }, [scrollY, controls]);
+  useLayoutEffect(() => {
+    controls.start({ opacity: 1 - scrollY / 150 });
+  }, [scrollY, controls]);
 
-  // useLayoutEffect(() => {
-  //   window.addEventListener("scroll", handleScroll);
+  useLayoutEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, [handleScroll]);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
 
   return (
-    <section className="fixed inset-0 z-0 flex flex-col justify-end h-[90svh] mx-auto w-full max-w-7xl px-4 xl:px-0 overflow-hidden">
+    <section className="fixed inset-0 z-0 flex flex-col justify-end h-[90svh] lg:h-[100svh] mx-auto w-full max-w-7xl px-8 xl:px-0 overflow-hidden">
       <p className="z-10 font-bold">
-        <motion.span
-          initial={{ x: -400 }}
-          animate={{ x: 0 }}
-          transition={{ delay: 0.3, duration: 0.5 }}
-          className="block mt-5 text-[6rem] md:text-[8rem]"
-        >
-          {/* <span className="relative">
+        <span className="block text-[5rem] md:text-[8rem]">
+          <span className="relative">
             <motion.span
               initial={{ opacity: 1 }}
               animate={controls}
               className="absolute z-10 top-0 bottom-0"
-            > */}
-          Hello,
-          {/* </motion.span> */}
-          {/* <span className="absolute z-0 top-0 bottom-0 text-red-500"> */}
-          {/* Hell */}
-          {/* </span> */}
-          {/* </span> */}
-          {/* &#8203; */}
-        </motion.span>
-        <motion.span
-          initial={{ x: 500, opacity: 0 }}
-          animate={{ x: 0, opacity: 1 }}
-          transition={{ delay: 0.8, duration: 0.7 }}
-          className="block text-[2.7rem] md:text-[5rem]"
-        >
+            >
+              Hello,
+            </motion.span>
+            <span className="absolute z-0 top-0 bottom-0 text-red-600">
+              Hell
+            </span>
+          </span>
+          &#8203;
+        </span>
+        <span className="block text-[2.7rem] md:text-[5rem]">
           I am Nikolaos
-        </motion.span>
-        <motion.span
-          initial={{ y: 100 }}
-          animate={{ y: 0 }}
-          transition={{ delay: 1.5, duration: 1 }}
-          className="block mt-10 text-[1rem] md:text-[2rem] text-gray-500"
-        >
+        </span>
+        <span className="block font-normal mt-8 text-[1rem] md:text-[2rem] text-zinc-500">
           A Frontend Developer based in Greece
-        </motion.span>
+        </span>
       </p>
     </section>
   );
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -8,7 +8,6 @@ const Main = () => {
     <main className="relative">
       <Hero />
       <Projects />
-      {/* <Cubic /> */}
     </main>
   );
 };
