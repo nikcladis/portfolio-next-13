913e20515a864dd9b692cec0b5c999c09feb86ed
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-24 21:14:39 +0200

    fixed background attachment fixed not working on safari

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,34 @@
+import React from "react";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
 
 export default function Home() {
   return (
-    <div
-      style={{
-        backgroundImage: "url(/background.jpg)",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundAttachment: "fixed",
-      }}
-    >
-      <div className="relative">
+    <div className="relative">
+      <div
+        className="background-container"
+        style={{
+          position: "relative",
+          zIndex: 1, // Ensure the content stays on top of the pseudo-element
+        }}
+      >
         <Header />
         <Main />
       </div>
+      <div
+        className="background-fix"
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundImage: `url(background.jpg)`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          zIndex: 0, // Ensure the pseudo-element stays behind the content
+        }}
+      />
     </div>
   );
 }
