3594e2ceef893e44dd58d136754eed45dc9b9027
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-24 21:21:03 +0200

    fixed background image height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           top: 0,
           left: 0,
           width: "100%",
-          height: "100%",
+          height: "100vh",
           backgroundImage: `url(background.jpg)`,
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
