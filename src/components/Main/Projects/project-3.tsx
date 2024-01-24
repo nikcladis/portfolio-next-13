74424330d29de158cfb1a6840c86aeaf1cab9ebe
Nikolaos Kladis <nikcladis@gmail.com>
2024-01-24 23:41:11 +0200

    removed margin x

--- src/components/Main/Projects/project-3.tsx
+++ src/components/Main/Projects/project-3.tsx
@@ -15,7 +15,7 @@ const SectionThree = () => {
       style={{ scale: scrollYProgress }}
       className="z-30 flex h-[100svh] mx-auto w-full max-w-7xl overflow-hidden sticky"
     >
-      <div className="mt-24 mx-8 max-w-full bg-gray-800 h-[80%] rounded-xl w-full flex flex-col md:flex-row gap-4">
+      <div className="mt-24 max-w-full bg-gray-800 h-[80%] rounded-xl w-full flex flex-col md:flex-row gap-4">
         {/* <div className="w-full md:w-1/2 bg-gray-800 h-[48%] md:h-full rounded-xl"></div>
         <div className="w-full md:w-1/4 bg-gray-600 h-[23%] md:h-full rounded-xl"></div>
         <div className="w-full md:w-1/4 bg-gray-600 h-[23%] md:h-full rounded-xl"></div> */}
